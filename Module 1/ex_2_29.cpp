28221e4713c69febbc34bb0c385ab2f71266b81e
Luis Miranda <61203868+luism1randa@users.noreply.github.com>
2021-01-15 13:19:31 -0700

    Completed ex 2.29

--- Module 1/ex_2_29.cpp
+++ Module 1/ex_2_29.cpp
@@ -4,12 +4,26 @@
 //
 //  Created by Luis Miranda
 
-//  This program...
+//  This program prints a table of integers from 0 to 10
+//  and their respective square and cubed values.
 //
 //
 
 #include <iostream>
 
 int main() {
+    std::cout << "integer square  cube" << std::endl;
     
+    int square;
+    int cube;
+    
+    for (int i = 0; i<=10; i++) {
+        std::cout << i << "\t\t";
+        
+        square = i * i;
+        std::cout << square << "\t\t";
+        
+        cube = i * i * i;
+        std::cout << cube << std::endl;
+    }
 }
